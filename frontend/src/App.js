39cddb948431166b9588ef9a5cc68d383efc1cac
siddhaviraul22 <raulsiddhavi678@gmail.com>
2026-01-03 19:34:51 +0530

    Day 11: Initialized React frontend with basic structure

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Splitly</h1>
+      <p>Expense Sharing App</p>
+    </div>
+  );
+}
+
+export default App;
