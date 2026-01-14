9c2a9501f391a620e827ddd7bba108aef07e4b0f
siddhaviraul22 <raulsiddhavi678@gmail.com>
2026-01-14 20:40:53 +0530

    Day 3: User entity, repository, and create user API

---
+++ backend/splitly-backend/src/main/java/com/splitly/backend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.splitly.backend.repository;
+
+import com.splitly.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
