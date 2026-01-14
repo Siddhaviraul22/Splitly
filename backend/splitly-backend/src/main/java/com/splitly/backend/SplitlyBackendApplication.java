8307a7662f5843f35ea1685d341d1d3956a8d469
siddhaviraul22 <raulsiddhavi678@gmail.com>
2026-01-14 16:36:55 +0530

    Day 1: Spring Boot backend project setup

---
+++ backend/splitly-backend/src/main/java/com/splitly/backend/SplitlyBackendApplication.java
@@ -0,0 +1,12 @@
+package com.splitly.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitlyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitlyBackendApplication.class, args);
+    }
+}
---
+++ backend/splitly-backend/src/test/java/com/splitly/backend/SplitlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitly.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
