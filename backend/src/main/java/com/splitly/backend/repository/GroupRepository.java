fce982e773071a8236c59242f41a32d5290d59c4
Siddhavi Raul <132128142+Siddhaviraul22@users.noreply.github.com>
2026-01-14 13:52:58 +0530

    Delete backend directory

--- backend/src/main/java/com/splitly/backend/repository/GroupRepository.java
+++
@@ -1,6 +0,0 @@
-package com.splitly.backend.repository;
-
-import com.splitly.backend.entity.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends JpaRepository<Group, Long> {}
