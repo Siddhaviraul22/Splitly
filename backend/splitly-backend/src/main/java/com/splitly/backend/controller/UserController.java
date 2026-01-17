5a9596a972083c2b839aa0db8343be13922be5ba
siddhaviraul22 <raulsiddhavi678@gmail.com>
2026-01-17 18:42:35 +0530

    Day 5: Password encryption using BCrypt

--- backend/splitly-backend/src/main/java/com/splitly/backend/controller/UserController.java
+++ backend/splitly-backend/src/main/java/com/splitly/backend/controller/UserController.java
@@ -1,21 +1,50 @@
 package com.splitly.backend.controller;
 
+import com.splitly.backend.dto.LoginRequest;
 import com.splitly.backend.entity.User;
 import com.splitly.backend.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository,
+                          PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping
     public User createUser(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+
+        Optional<User> optionalUser =
+                userRepository.findByEmail(request.getEmail());
+
+        if (optionalUser.isEmpty()) {
+            return "User not found";
+        }
+
+        User user = optionalUser.get();
+
+        if (!passwordEncoder.matches(
+                request.getPassword(),
+                user.getPassword())) {
+            return "Invalid password";
+        }
+
+        return "Login successful";
     }
 }
